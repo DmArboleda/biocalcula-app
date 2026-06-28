b2af3c48919203a518896a50abb19e505aa871dc
DmArboleda <dmarboleda@uce.edu.ec>
2026-06-28 17:28:42 -0500

    feat: agregar entidad Usuario, DAO y base de datos Room

---
+++ app/src/main/java/com/arboleda/biocalcula/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.arboleda.biocalcula
+
+class LoginActivity {
+}
---
+++ app/src/main/java/com/arboleda/biocalcula/UsuarioDao.kt
@@ -0,0 +1,18 @@
+package com.arboleda.biocalcula
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UsuarioDao {
+
+    @Insert
+    suspend fun insertarUsuario(usuario: Usuario)
+
+    @Query("SELECT * FROM usuario WHERE correo = :correo AND contrasena = :contrasena LIMIT 1")
+    suspend fun login(correo: String, contrasena: String): Usuario?
+
+    @Query("SELECT * FROM usuario WHERE correo = :correo LIMIT 1")
+    suspend fun buscarPorCorreo(correo: String): Usuario?
+}
